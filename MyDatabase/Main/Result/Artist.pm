f730678c7f693fa72df2715f59de415bc6c108f0
Alex Kalderimis <alex.kalderimis@gmail.com>
2010-12-06 15:47:30 +0000

    Added the files

---
+++ MyDatabase/Main/Result/Artist.pm
@@ -0,0 +1,8 @@
+  package MyDatabase::Main::Result::Artist;
+  use base qw/DBIx::Class::Core/;
+  __PACKAGE__->table('artist');
+  __PACKAGE__->add_columns(qw/ artistid name /);
+  __PACKAGE__->set_primary_key('artistid');
+  __PACKAGE__->has_many('cds' => 'MyDatabase::Main::Result::Cd');
+
+  1;
